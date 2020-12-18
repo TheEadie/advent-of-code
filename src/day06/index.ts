beb6f5f8dab2d6ff8122bf32f3ddb3e83cec0590
David Eadie <david@eadie.net>
2020-12-18 20:24:23 +0000

    Day 6

---
+++ src/day06/index.ts
@@ -0,0 +1,43 @@
+import { readFileSync } from "fs";
+
+const file = readFileSync("./src/day06/input.txt", "utf-8").split("\n\n");
+
+const partOne = (input: string[]): number => {
+  return input.map(atLeastOneAnsweredYes).reduce((acc, val) => {
+    acc += val;
+    return acc;
+  });
+};
+
+const partTwo = (input: string[]): number => {
+  return input.map(allAnsweredYes).reduce((acc, val) => {
+    acc += val;
+    return acc;
+  });
+};
+
+const atLeastOneAnsweredYes = (input: string): number => {
+  const letterTally = input.split("").reduce((acc, char) => {
+    if (char === "\n") return acc;
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.keys(letterTally).length;
+};
+
+const allAnsweredYes = (input: string): number => {
+  const numberOfPeople = input.split("\n").length;
+  const letterTally = input.split("").reduce((acc, char) => {
+    if (char === "\n") return acc;
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.keys(letterTally).filter(
+    (x) => letterTally[x] === numberOfPeople
+  ).length;
+};
+
+console.log(`Part 1: ${partOne(file)}`);
+console.log(`Part 2: ${partTwo(file)}`);
