dd0b5a481ba8127e363c561088cb05b26e835f0c
David Eadie <david.eadie@red-gate.com>
2021-11-25 15:58:23 +0000

    Add framework for C#

---
+++ 2021/c#/AdventOfCode2021/AdventOfCode2021/Day01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode2021
+{
+    public class Day01
+    {
+        [Test]
+        public void Part1()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
