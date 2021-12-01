3b731d2ec8d61241716d8e1e4dc919524cf9eeef
David Eadie <david.eadie@red-gate.com>
2021-12-01 09:41:20 +0000

    Terrible implementation of Day01
    Need to brush up on my LINQ

--- 2021/c#/AdventOfCode2021/AdventOfCode2021/Day01.cs
+++ 2021/c#/AdventOfCode2021/AdventOfCode2021/Day01.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using NUnit.Framework;
 
 namespace AdventOfCode2021
@@ -8,7 +11,47 @@ namespace AdventOfCode2021
         [Test]
         public void Part1()
         {
-            Console.WriteLine("Hello World");
+            var lines = File.ReadAllLines("Day01.txt");
+            var depths = lines.Select(int.Parse).ToArray();
+
+            var count = 0;
+            var previous = int.MaxValue;
+
+            foreach (var depth in depths)
+            {
+                if (depth > previous)
+                    count++;
+                previous = depth;
+            }
+
+            Console.WriteLine(count);
+        }
+        
+        [Test]
+        public void Part2()
+        {
+            var lines = File.ReadAllLines("Day01.txt");
+            var depths = lines.Select(int.Parse).ToArray();
+
+            var slidingWindows = new List<int>();
+            
+            for (var i = 0; i < depths.Length - 2; i++)
+            {
+                var sum = depths[i] + depths[i + 1] + depths[i + 2];
+                slidingWindows.Add(sum);
+            }
+
+            var count = 0;
+            var previous = int.MaxValue;
+
+            foreach (var depth in slidingWindows)
+            {
+                if (depth > previous)
+                    count++;
+                previous = depth;
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
