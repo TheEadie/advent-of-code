3a333fe351a9b6de68005a5ce879112e3f04f0ac
David Eadie <david.eadie@red-gate.com>
2021-12-01 12:00:13 +0000

    Refactor using zip

--- 2021/c#/AdventOfCode2021/AdventOfCode2021/Day01.cs
+++ 2021/c#/AdventOfCode2021/AdventOfCode2021/Day01.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
+using Shouldly;
 
 namespace AdventOfCode2021
 {
@@ -11,47 +11,39 @@ namespace AdventOfCode2021
         [Test]
         public void Part1()
         {
-            var lines = File.ReadAllLines("Day01.txt");
-            var depths = lines.Select(int.Parse).ToArray();
+            var depths = ParseInput();
 
-            var count = 0;
-            var previous = int.MaxValue;
-
-            foreach (var depth in depths)
-            {
-                if (depth > previous)
-                    count++;
-                previous = depth;
-            }
-
+            var count = depths
+                .Zip(depths.Skip(1), (prev, current) => current > prev)
+                .Count(x => x);
+            
             Console.WriteLine(count);
+            count.ShouldBe(1292);
         }
-        
+
         [Test]
         public void Part2()
         {
-            var lines = File.ReadAllLines("Day01.txt");
-            var depths = lines.Select(int.Parse).ToArray();
+            var depths = ParseInput();
 
-            var slidingWindows = new List<int>();
-            
-            for (var i = 0; i < depths.Length - 2; i++)
-            {
-                var sum = depths[i] + depths[i + 1] + depths[i + 2];
-                slidingWindows.Add(sum);
-            }
+            var slidingWindows = depths
+                .Zip(depths.Skip(1), (first, second) => first + second)
+                .Zip(depths.Skip(2), (sum, third) => sum + third)
+                .ToArray();
 
-            var count = 0;
-            var previous = int.MaxValue;
-
-            foreach (var depth in slidingWindows)
-            {
-                if (depth > previous)
-                    count++;
-                previous = depth;
-            }
+            var count = slidingWindows
+                .Zip(slidingWindows.Skip(1), (prev, current) => current > prev)
+                .Count(x => x);
 
             Console.WriteLine(count);
+            count.ShouldBe(1262);
+        }
+
+        private static int[] ParseInput()
+        {
+            var lines = File.ReadAllLines("Day01.txt");
+            var depths = lines.Select(int.Parse).ToArray();
+            return depths;
         }
     }
 }
