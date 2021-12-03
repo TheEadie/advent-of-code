f532664054f1320f0802ec9e970c9f6af1fe95e1
David Eadie <david@eadie.net>
2021-12-03 11:05:37 +0000

    Refactor day 3

--- 2021/c#/AdventOfCode2021/AdventOfCode2021/Day03.cs
+++ 2021/c#/AdventOfCode2021/AdventOfCode2021/Day03.cs
@@ -12,38 +12,20 @@ namespace AdventOfCode2021
         [Test]
         public void Part1()
         {
-            var grid = ParseInput();
+            var input = ParseInput();
             var gamma = string.Empty;
-            var epsilon = string.Empty;
 
-
-            for (var j = 0; j < grid[0].Length; j++)
+            for (var position = 0; position < input[0].Length; position++)
             {
-                var ones = 0;
-                var zeros = 0;
-                for (var i = 0; i < grid.Length; i++)
-                {
-                    if (grid[i][j] == '1')
-                        ones++;
-                    if (grid[i][j] == '0')
-                        zeros++;
-                }
-
-                if (ones > zeros)
-                {
-                    gamma += '1';
-                    epsilon += '0';
-                }
-                else
-                {
-                    gamma += '0';
-                    epsilon += '1';
-                }
+                var ones = NumberOfBits(input, '1', position);
+                var zeros = NumberOfBits(input, '0', position);
+                gamma += ones > zeros ? '1' : '0';
             }
+
+            var epsilon = new string(gamma.Select(x => x == '1' ? '0' : '1').ToArray());
 
             var gammaAsNumber = Convert.ToInt32(gamma, 2);
             var epsilonAsNumber = Convert.ToInt32(epsilon, 2);
-
 
             var answer = gammaAsNumber * epsilonAsNumber;
             Console.WriteLine(answer);
@@ -53,85 +35,31 @@ namespace AdventOfCode2021
         [Test]
         public void Part2()
         {
-            var grid = ParseInput();
+            var oxygenInput = ParseInput().ToList();
 
-            // for each position j
-            // lines = most common of 0 or 1 at current position
-
-
-            for (var j = 0; j < grid[0].Length; j++)
+            for (var position = 0; position < oxygenInput[0].Length; position++)
             {
-                var ones = 0;
-                var zeros = 0;
-                var binary1 = new List<char[]>();
-                var binary0 = new List<char[]>();
+                var ones = NumberOfBits(oxygenInput, '1', position);
+                var zeros = NumberOfBits(oxygenInput, '0', position);
+                oxygenInput.RemoveAll(x => x[position] == (ones >= zeros ? '0' : '1'));
 
-                for (var i = 0; i < grid.Length; i++)
-                {
-                    if (grid[i][j] == '1')
-                    {
-                        ones++;
-                        binary1.Add(grid[i]);
-                    }
-
-                    if (grid[i][j] == '0')
-                    {
-                        zeros++;
-                        binary0.Add(grid[i]);
-                    }
-                }
-
-                if (ones >= zeros)
-                {
-                    grid = binary1.ToArray();
-                }
-                else
-                {
-                    grid = binary0.ToArray();
-                }
+                if (oxygenInput.Count == 1) break;
             }
 
-            var oxygen = new string(grid[0]);
+            var oxygen = new string(oxygenInput[0]);
 
-            grid = ParseInput();
+            var co2Input = ParseInput().ToList();
 
-            for (var j = 0; j < grid[0].Length; j++)
+            for (var position = 0; position < co2Input[0].Length; position++)
             {
-                var ones = 0;
-                var zeros = 0;
-                var binary1 = new List<char[]>();
-                var binary0 = new List<char[]>();
+                var ones = NumberOfBits(co2Input, '1', position);
+                var zeros = NumberOfBits(co2Input, '0', position);
+                co2Input.RemoveAll(x => x[position] == (ones >= zeros ? '1' : '0'));
 
-                for (var i = 0; i < grid.Length; i++)
-                {
-                    if (grid[i][j] == '1')
-                    {
-                        ones++;
-                        binary1.Add(grid[i]);
-                    }
-
-                    if (grid[i][j] == '0')
-                    {
-                        zeros++;
-                        binary0.Add(grid[i]);
-                    }
-                }
-
-                if (ones >= zeros)
-                {
-                    if (binary0.Count == 0)
-                        break;
-                    grid = binary0.ToArray();
-                }
-                else
-                {
-                    if (binary1.Count == 0)
-                        break;
-                    grid = binary1.ToArray();
-                }
+                if (co2Input.Count == 1) break;
             }
 
-            var co2 = new string(grid[0]);
+            var co2 = new string(co2Input[0]);
 
             var oxygenAsNumber = Convert.ToInt32(oxygen, 2);
             var co2AsNumber = Convert.ToInt32(co2, 2);
@@ -146,6 +74,11 @@ namespace AdventOfCode2021
             var lines = File.ReadAllLines("Day03.txt");
             var characters = lines.Select(x => x.ToCharArray()).ToArray();
             return characters;
+        }
+
+        private static int NumberOfBits(IEnumerable<char[]> input, char bit, int position)
+        {
+            return input.Select(x => x[position]).Count(x => x == bit);
         }
     }
 }
