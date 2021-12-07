5e2a77ac7fd760c20de6c1dbe0e319f48bd25f36
David Eadie <david@eadie.net>
2021-12-07 13:28:08 +0000

    2015 Day 1

---
+++ 2015/c#/AdventOfCode/Day01.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode
+{
+    public class Day01
+    {
+        [Test]
+        public void Part1()
+        {
+            var moves = ParseInput();
+
+            var answer = moves.Sum();
+            Console.WriteLine(answer);
+            answer.ShouldBe(138);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var moves = ParseInput();
+            var currentFloor = 0;
+            var answer = 0;
+            for(var i = 0; i < moves.Length; i++)
+            {
+                currentFloor += moves[i];
+                if (currentFloor == -1)
+                {
+                    answer = i + 1;
+                    break;
+                }
+            }
+
+            Console.WriteLine(answer);
+            answer.ShouldBe(1771);
+        }
+
+        private static int[] ParseInput()
+        {
+            var lines = File.ReadAllLines("Day01.txt");
+            return lines[0].ToCharArray().Select(x => (x == '(') ? 1 : -1).ToArray();
+        }
+    }
+}
