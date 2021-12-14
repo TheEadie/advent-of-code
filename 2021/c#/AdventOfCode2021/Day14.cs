05ad31debc2e7f8cca8daee4a0e3c43ff2aa3540
David Eadie <david@eadie.net>
2021-12-14 12:17:30 +0000

    Day 14 Part 2

--- 2021/c#/AdventOfCode2021/Day14.cs
+++ 2021/c#/AdventOfCode2021/Day14.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using AdventOfCode2021.Utils;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 using Shouldly;
 
 namespace AdventOfCode2021
@@ -16,33 +14,15 @@ namespace AdventOfCode2021
         {
             var (template, rules) = ParseInput();
 
-            var sequence = Run(template, rules).Take(10).Last();
+            var elementCounts = CountElements(template, rules, 10);
 
-            var group = sequence.GroupBy(x => x);
-            var mostCommon = group.OrderBy(x => x.Count()).Last();
-            var leastCommon = group.OrderBy(x => x.Count()).First();
+            var mostCommon = elementCounts.MaxBy(x => x.Value).Value;
+            var leastCommon = elementCounts.MinBy(x => x.Value).Value;
 
-            var answer = mostCommon.Count() - leastCommon.Count();
+            var answer = mostCommon - leastCommon;
 
             Console.WriteLine(answer);
             answer.ShouldBe(2345);
-        }
-
-        private static IEnumerable<string> Run(string template, IDictionary<string, string> rules)
-        {
-            while (true)
-            {
-                var result = template
-                    .Zip(template.Skip(1), (curr, next) =>
-                    {
-                        var key = $"{curr}{next}";
-                        return (rules.ContainsKey(key) ? $"{curr}{rules[key]}" : $"{curr}");
-                    });
-
-                template = string.Join("", result) + template.Last();
-
-                yield return template;
-            }
         }
 
         [Test]
@@ -50,21 +30,65 @@ namespace AdventOfCode2021
         {
             var (template, rules) = ParseInput();
 
-            var sequence = Run(template, rules).Take(40).Last();
+            var elementCounts = CountElements(template, rules, 40);
 
-            var group = sequence.GroupBy(x => x);
-            var mostCommon = group.OrderBy(x => x.Count()).Last();
-            var leastCommon = group.OrderBy(x => x.Count()).First();
+            var mostCommon = elementCounts.MaxBy(x => x.Value).Value;
+            var leastCommon = elementCounts.MinBy(x => x.Value).Value;
 
-            long answer = mostCommon.Count() - leastCommon.Count();
+            var answer = mostCommon - leastCommon;
 
             Console.WriteLine(answer);
-            answer.ShouldBe(2345);
+            answer.ShouldBe(2432786807053);
+        }
+
+        private static Dictionary<char, long> CountElements(string template, IDictionary<string, char> rules, int iterations)
+        {
+            var initialPairCount = new Dictionary<string, long>();
+
+            for (var i = 0; i < template.Length - 1; i++)
+            {
+                var ab = template.Substring(i, 2);
+                initialPairCount[ab] = initialPairCount.GetValueOrDefault(ab) + 1;
+            }
+
+            var sequence = Run(initialPairCount, rules).Take(iterations).Last();
+
+            var elementCounts = new Dictionary<char, long>();
+
+            foreach (var molecule in sequence)
+            {
+                var element = molecule.Key[0];
+                elementCounts[element] = elementCounts.GetValueOrDefault(element) + molecule.Value;
+            }
+
+            elementCounts[template.Last()] = elementCounts.GetValueOrDefault(template.Last()) + 1;
+            return elementCounts;
+        }
+
+        private static IEnumerable<IDictionary<string, long>> Run(IDictionary<string, long> pairCounts, IDictionary<string, char> rules)
+        {
+            while (true)
+            {
+                var newPairCounts = new Dictionary<string, long>();
+
+                foreach (var pairCount in pairCounts)
+                {
+                    var a = pairCount.Key[0];
+                    var b = pairCount.Key[1];
+                    var n = rules[pairCount.Key];
+
+                    newPairCounts[$"{a}{n}"] = newPairCounts.GetValueOrDefault($"{a}{n}") + pairCount.Value;
+                    newPairCounts[$"{n}{b}"] = newPairCounts.GetValueOrDefault($"{n}{b}") + pairCount.Value;
+                }
+
+                pairCounts = newPairCounts;
+
+                yield return newPairCounts;
+            }
         }
 
 
-
-        private static (string, IDictionary<string, string>) ParseInput()
+        private static (string, IDictionary<string, char>) ParseInput()
         {
 
             var lines = File.ReadAllLines("Day14.txt");
@@ -74,7 +98,7 @@ namespace AdventOfCode2021
             var rules = lines.Skip(2)
                 .Select(x => x.Split(" -> "))
                 .Select(x => new KeyValuePair<string, string>(x[0], x[1]))
-                .ToDictionary(rule => rule.Key, rule => rule.Value);
+                .ToDictionary(rule => rule.Key, rule => rule.Value[0]);
 
             return (template, rules);
         }
