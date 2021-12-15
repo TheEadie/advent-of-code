fa9f7fb43091455ffb19380273f7a75bd41bddf4
David Eadie <david@eadie.net>
2021-12-15 12:14:13 +0000

    Day 15 Part 2

--- 2021/c#/AdventOfCode2021/Day15.cs
+++ 2021/c#/AdventOfCode2021/Day15.cs
@@ -81,7 +81,74 @@ namespace AdventOfCode2021
         [Test]
         public void Part2()
         {
+            var input = ParseInput();
+            var riskPlane = new Dictionary<Coordinate, int>();
 
+            var width = input.MaxBy(x => x.Key.X).Key.X + 1;
+            var height = input.MaxBy(x => x.Key.Y).Key.Y + 1;
+
+            foreach (var (key, value) in input)
+            {
+                for (var x = 0; x < 5; x++)
+                {
+                    for (var y = 0; y < 5; y++)
+                    {
+                        var cell = new Coordinate(key.X + (x * width), key.Y + (y * height));
+
+                        var newRisk = (value + x + y);
+                        if (newRisk > 9)
+                        {
+                            newRisk = (newRisk % 10) + 1;
+                        }
+
+                        riskPlane.Add(cell, newRisk);
+                    }
+                }
+            }
+
+            var start = riskPlane.First().Key;
+            var goal = riskPlane.Last().Key;
+
+            var queue = new PriorityQueue<Coordinate, int>();
+
+            var gScore = new Dictionary<Coordinate, int>();
+            gScore[start] = 0;
+
+            var cameFrom = new Dictionary<Coordinate, Coordinate>();
+
+            var totalPath = new List<Coordinate> { goal };
+
+            queue.Enqueue(start, DistanceToGoal(start, goal));
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                if (current == goal)
+                {
+                    while (cameFrom.ContainsKey(current))
+                    {
+                        current = cameFrom[current];
+                        totalPath = totalPath.Prepend(current).ToList();
+                    }
+                    break;
+                }
+
+                foreach (var neighbour in GetNeighbours(current, riskPlane))
+                {
+                    var tentativeGScore = gScore[current] + riskPlane[neighbour];
+                    if (!gScore.ContainsKey(neighbour) || tentativeGScore < gScore[neighbour])
+                    {
+                        cameFrom[neighbour] = current;
+                        gScore[neighbour] = tentativeGScore;
+                        queue.Enqueue(neighbour, tentativeGScore + DistanceToGoal(neighbour, goal));
+                    }
+                }
+            }
+
+            var answer = gScore[goal];
+            Console.WriteLine(answer);
+            answer.ShouldBe(373);
         }
 
         private static Dictionary<Coordinate, int> ParseInput()
@@ -92,7 +159,6 @@ namespace AdventOfCode2021
 
             var sizeX = rows.Count;
             var sizeY = rows[0].Count();
-            var heightMap = new int[sizeX, sizeY];
 
             for (var x = 0; x < sizeX; x++)
             {
