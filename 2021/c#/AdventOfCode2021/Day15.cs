4bdc21fef5152f657c86637d96395ab8128b611c
David Eadie <david@eadie.net>
2021-12-15 11:40:12 +0000

    Day 15 Part 1

---
+++ 2021/c#/AdventOfCode2021/Day15.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventOfCode2021.Utils;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode2021
+{
+    public class Day15
+    {
+        [Test]
+        public void Part1()
+        {
+            var riskPlane = ParseInput();
+
+            var start = riskPlane.First().Key;
+            var goal = riskPlane.Last().Key;
+
+            var queue = new PriorityQueue<Coordinate, int>();
+
+            var gScore = new Dictionary<Coordinate, int>();
+            gScore[start] = 0;
+
+            var cameFrom = new Dictionary<Coordinate, Coordinate>();
+
+            var totalPath = new List<Coordinate> { goal };
+
+            queue.Enqueue(start, DistanceToGoal(start, goal));
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                if (current == goal)
+                {
+                    while (cameFrom.ContainsKey(current))
+                    {
+                        current = cameFrom[current];
+                        totalPath = totalPath.Prepend(current).ToList();
+                    }
+                    break;
+                }
+
+                foreach (var neighbour in GetNeighbours(current, riskPlane))
+                {
+                    var tentativeGScore = gScore[current] + riskPlane[neighbour];
+                    if (!gScore.ContainsKey(neighbour) || tentativeGScore < gScore[neighbour])
+                    {
+                        cameFrom[neighbour] = current;
+                        gScore[neighbour] = tentativeGScore;
+                        queue.Enqueue(neighbour, tentativeGScore + DistanceToGoal(neighbour, goal));
+                    }
+                }
+            }
+
+            var answer = gScore[goal];
+            Console.WriteLine(answer);
+            answer.ShouldBe(373);
+        }
+
+        private int DistanceToGoal(Coordinate current, Coordinate goal)
+        {
+            return (goal.X - current.X) + (goal.Y - current.Y);
+        }
+
+        private static IEnumerable<Coordinate> GetNeighbours(Coordinate input, IReadOnlyDictionary<Coordinate, int> map)
+        {
+            var testCoordinates = new List<Coordinate>
+            {
+                new(input.X - 1, input.Y),
+                new(input.X, input.Y - 1),
+                new(input.X, input.Y + 1),
+                new(input.X + 1, input.Y)
+            };
+
+            return testCoordinates.Where(map.ContainsKey);
+        }
+
+        [Test]
+        public void Part2()
+        {
+
+        }
+
+        private static Dictionary<Coordinate, int> ParseInput()
+        {
+            var map = new Dictionary<Coordinate, int>();
+            var lines = File.ReadAllLines("Day15.txt");
+            var rows = lines.Select(line => line.ToCharArray().Select(x => int.Parse(x.ToString())).ToList()).ToList();
+
+            var sizeX = rows.Count;
+            var sizeY = rows[0].Count();
+            var heightMap = new int[sizeX, sizeY];
+
+            for (var x = 0; x < sizeX; x++)
+            {
+                for (var y = 0; y < sizeY; y++)
+                {
+                    map.Add(new Coordinate(x, y), rows[x][y]);
+                }
+            }
+
+            return map;
+        }
+    }
+}
