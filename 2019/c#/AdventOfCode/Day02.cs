937a25a5c8953412d307dde83d2e4321c3945e31
David Eadie <david@eadie.net>
2022-07-26 13:43:44 +0100

    2019 - Day 2

---
+++ 2019/c#/AdventOfCode/Day02.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode
+{
+    public class Day02
+    {
+        [Test]
+        public void Part1()
+        {
+            var program = File.ReadAllLines("day02.txt")[0]
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+
+            program[1] = 12;
+            program[2] = 2;
+            
+            var emulator = new IntCode.IntCode(program);
+            emulator.Run();
+            var answer = emulator.Memory[0];
+
+            Console.WriteLine(answer);
+            answer.ShouldBe(3790645);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            (int noun, int verb) FindResult(int[] program, int result)
+            {
+                for (var n = 0; n < 100; n++)
+                {
+                    for (var v = 0; v < 100; v++)
+                    {
+                        program[1] = n;
+                        program[2] = v;
+                        var emulator = new IntCode.IntCode(program);
+                        emulator.Run();
+                        var output = emulator.Memory[0];
+
+                        if (output == result)
+                            return (n, v);
+                    }
+                }
+
+                throw new Exception($"No inputs found that result in {result}");
+            }
+
+            var program = File.ReadAllLines("day02.txt")[0]
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+            
+            var (noun, verb) = FindResult(program, 19690720);
+
+            var answer = 100 * noun + verb;
+            Console.WriteLine(answer);
+            answer.ShouldBe(6577);
+        }
+    }
+}
---
+++ 2019/c#/AdventOfCode/IntCode/IntCode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.IntCode;
+
+public class IntCode
+{
+    private int _pc;
+    
+    public int[] Memory { get; }
+    public Stack<int> Inputs { get; }
+    public List<int> Output { get; }
+
+    public IntCode(int[] program)
+    {
+        Inputs = new Stack<int>();
+        Output = new List<int>();
+        Memory = (int[]) program.Clone();
+    }
+
+    public void Run()
+    {
+        _pc = 0;
+
+        while (Memory[_pc] != 99)
+        {
+            var opCode = Memory[_pc];
+
+            switch (opCode)
+            {
+                case 1:
+                    Add();
+                    break;
+                case 2:
+                    Multiply();
+                    break;
+                default:
+                    throw new Exception("Unknown op code");
+            }
+        }
+    }
+
+    private void Add()
+    {
+        var a = Memory[Memory[_pc + 1]];
+        var b = Memory[Memory[_pc + 2]];
+        var c = Memory[_pc + 3];
+        Memory[c] = a + b;
+        _pc += 4;
+    }
+
+    private void Multiply()
+    {
+        var a = Memory[Memory[_pc + 1]];
+        var b = Memory[Memory[_pc + 2]];
+        var c = Memory[_pc + 3];
+        Memory[c] = a * b;
+        _pc += 4;
+    }
+}
