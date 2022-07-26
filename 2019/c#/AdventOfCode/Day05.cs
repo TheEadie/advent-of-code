1b6d274b375dce8460ef11926abc1c2536773543
David Eadie <david@eadie.net>
2022-07-26 14:53:15 +0100

    2019 - Day 5

---
+++ 2019/c#/AdventOfCode/Day05.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode
+{
+    public class Day05
+    {
+        [Test]
+        public void Part1()
+        {
+            var program = File.ReadAllLines("day05.txt")[0]
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+
+            var emulator = new IntCode.IntCode(program);
+            
+            emulator.Inputs.Enqueue(1);
+
+            emulator.Run();
+
+            foreach (var value in emulator.Output)
+            {
+                Console.WriteLine(value);
+            }
+
+            var answer = emulator.Output.Last();
+            answer.ShouldBe(16225258);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var program = File.ReadAllLines("day05.txt")[0]
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+
+            var emulator = new IntCode.IntCode(program);
+            
+            emulator.Inputs.Enqueue(5);
+
+            emulator.Run();
+
+            foreach (var value in emulator.Output)
+            {
+                Console.WriteLine(value);
+            }
+
+            var answer = emulator.Output.Last();
+            answer.ShouldBe(2808771);
+        }
+    }
+}
--- 2019/c#/AdventOfCode/IntCode/IntCode.cs
+++ 2019/c#/AdventOfCode/IntCode/IntCode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace AdventOfCode.IntCode;
 
@@ -8,12 +9,12 @@ public class IntCode
     private int _pc;
     
     public int[] Memory { get; }
-    public Stack<int> Inputs { get; }
+    public Queue<int> Inputs { get; }
     public List<int> Output { get; }
 
     public IntCode(int[] program)
     {
-        Inputs = new Stack<int>();
+        Inputs = new Queue<int>();
         Output = new List<int>();
         Memory = (int[]) program.Clone();
     }
@@ -24,37 +25,133 @@ public class IntCode
 
         while (Memory[_pc] != 99)
         {
-            var opCode = Memory[_pc];
+            var opCode = Memory[_pc] % 100;
+            var modeA = (ParamMode)((Memory[_pc] / 100) % 10);
+            var modeB = (ParamMode)((Memory[_pc] / 1000) % 10);
+            var modeC = (ParamMode)((Memory[_pc] / 10000) % 10);
 
             switch (opCode)
             {
                 case 1:
-                    Add();
+                    Add(modeA, modeB, modeC);
                     break;
                 case 2:
-                    Multiply();
+                    Multiply(modeA, modeB, modeC);
+                    break;
+                case 3:
+                    ReadInput(modeA);
+                    break;
+                case 4:
+                    WriteOutput(modeA);
+                    break;
+                case 5:
+                    JumpIfTrue(modeA, modeB);
+                    break;
+                case 6:
+                    JumpIfFalse(modeA, modeB);
+                    break;
+                case 7:
+                    LessThan(modeA, modeB, modeC);
+                    break;
+                case 8:
+                    Equals(modeA, modeB, modeC);
                     break;
                 default:
-                    throw new Exception("Unknown op code");
+                    throw new Exception($"Unknown op code {opCode}");
             }
         }
     }
 
-    private void Add()
+    private enum ParamMode
     {
-        var a = Memory[Memory[_pc + 1]];
-        var b = Memory[Memory[_pc + 2]];
+        Position,
+        Immediate
+    }
+
+    private int GetValue(ParamMode mode, int location)
+    {
+        return mode == ParamMode.Position ? Memory[location] : location;
+    }
+
+    private void Add(ParamMode modeA, ParamMode modeB, ParamMode modeC)
+    {
+        var a = GetValue(modeA, Memory[_pc + 1]);
+        var b = GetValue(modeB, Memory[_pc + 2]);
         var c = Memory[_pc + 3];
         Memory[c] = a + b;
         _pc += 4;
     }
 
-    private void Multiply()
+    private void Multiply(ParamMode modeA, ParamMode modeB, ParamMode modeC)
     {
-        var a = Memory[Memory[_pc + 1]];
-        var b = Memory[Memory[_pc + 2]];
+        var a = GetValue(modeA, Memory[_pc + 1]);
+        var b = GetValue(modeB, Memory[_pc + 2]);
         var c = Memory[_pc + 3];
         Memory[c] = a * b;
+        _pc += 4;
+    }
+
+    private void ReadInput(ParamMode modeA)
+    {
+        var a = Memory[_pc + 1];
+        Memory[a] = Inputs.Dequeue();
+        _pc += 2;
+    }
+
+    private void WriteOutput(ParamMode modeA)
+    {
+        var a = GetValue(modeA, Memory[_pc + 1]);
+        Output.Add(a);
+        _pc += 2;
+    }
+
+    private void JumpIfTrue(ParamMode modeA, ParamMode modeB)
+    {
+        var a = GetValue(modeA, Memory[_pc + 1]);
+        var b = GetValue(modeB, Memory[_pc + 2]);
+
+        if (a != 0)
+        {
+            _pc = b;
+        }
+        else
+        {
+            _pc += 3;
+        }
+    }
+    
+    private void JumpIfFalse(ParamMode modeA, ParamMode modeB)
+    {
+        var a = GetValue(modeA, Memory[_pc + 1]);
+        var b = GetValue(modeB, Memory[_pc + 2]);
+
+        if (a == 0)
+        {
+            _pc = b;
+        }
+        else
+        {
+            _pc += 3;
+        }
+    }
+    
+    private void LessThan(ParamMode modeA, ParamMode modeB, ParamMode modeC)
+    {
+        var a = GetValue(modeA, Memory[_pc + 1]);
+        var b = GetValue(modeB, Memory[_pc + 2]);
+        var c = Memory[_pc + 3];
+
+        Memory[c] = a < b ? 1 : 0;
+        _pc += 4;
+    }
+    
+    private void Equals(ParamMode modeA, ParamMode modeB, ParamMode modeC)
+    {
+        var a = GetValue(modeA, Memory[_pc + 1]);
+        var b = GetValue(modeB, Memory[_pc + 2]);
+        var c = Memory[_pc + 3];
+
+        Memory[c] = a == b ? 1 : 0;
         _pc += 4;
     }
 }
