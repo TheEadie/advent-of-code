cce00269d9a4281ea63f8def67547c9344b9ab87
David Eadie <david@eadie.net>
2022-07-27 12:36:27 +0100

    2019 - Day 9

--- 2019/c#/AdventOfCode/Day09.cs
+++ 2019/c#/AdventOfCode/Day09.cs
@@ -29,8 +29,30 @@ namespace AdventOfCode
                 Console.WriteLine(output);
             }
 
-            //Console.WriteLine(answer);
-            //answer.ShouldBe(212460);
+            var answer = emulator.Output.Last();
+            answer.ShouldBe(2941952859);
+        }
+        
+        [Test]
+        public async Task Part2()
+        {
+            var program = (await File.ReadAllLinesAsync("day09.txt"))[0]
+                .Split(',')
+                .Select(long.Parse)
+                .ToArray();
+
+            var emulator = new IntCode.IntCode(program);
+            emulator.Inputs.Enqueue(2);
+
+            emulator.Run();
+
+            foreach (var output in emulator.Output)
+            {
+                Console.WriteLine(output);
+            }
+
+            var answer = emulator.Output.Last();
+            answer.ShouldBe(66113);
         }
     }
 }
