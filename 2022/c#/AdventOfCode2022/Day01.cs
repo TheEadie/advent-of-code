fcdba1b8c4a4329e6b5b583f17d2b9afabb20ad2
David Eadie <david@eadie.net>
2022-12-01 12:02:33 +0000

    2022 - Day 1 test samples as well

--- 2022/c#/AdventOfCode2022/Day01.cs
+++ 2022/c#/AdventOfCode2022/Day01.cs
@@ -1,24 +1,24 @@
-using Shouldly;
-
 namespace AdventOfCode2022;
 
 public class Day01
 {
-    [Test]
-    public void Part1()
+    [TestCase("data/01 - Sample.txt", 24000, TestName = "Sample")]
+    [TestCase("data/01 - Puzzle Input.txt", 69795, TestName = "Puzzle Input")]
+    public void Part1(string inputFile, int expected)
     {
-        var elves = ParseInput(File.ReadAllLines("Day01.txt"));
+        var elves = ParseInput(File.ReadAllLines(inputFile));
 
         var answer = elves.Max(x => x.Calories.Sum());
         
         Console.WriteLine(answer);
-        answer.ShouldBe(69795);
+        answer.ShouldBe(expected);
     }
     
-    [Test]
-    public void Part2()
+    [TestCase("data/01 - Sample.txt", 45000, TestName = "Sample")]
+    [TestCase("data/01 - Puzzle Input.txt", 208437, TestName = "Puzzle Input")]
+    public void Part2(string inputFile, int expected)
     {
-        var elves = ParseInput(File.ReadAllLines("Day01.txt"));
+        var elves = ParseInput(File.ReadAllLines(inputFile));
 
         var answer = elves.OrderByDescending(x => x.Calories.Sum())
             .Take(3)
@@ -26,29 +26,43 @@ public class Day01
             .Sum();
         
         Console.WriteLine(answer);
-        answer.ShouldBe(208437);
+        answer.ShouldBe(expected);
     }
 
     private static IEnumerable<Elf> ParseInput(IEnumerable<string> input)
     {
-        var builder = new List<int>();
+        var builder = new ElfBuilder();
         var elves = new List<Elf>();
 
         foreach (var line in input)
         {
             if (string.IsNullOrEmpty(line))
             {
-                elves.Add(new Elf(builder));
-                builder = new List<int>();
+                elves.Add(builder.Build());
+                builder = new ElfBuilder();
             }
             else
             {
                 builder.Add(int.Parse(line));
             }
         }
-
+        
+        // Add the last elf
+        elves.Add(builder.Build());
         return elves;
     }
 
-    private record Elf(IEnumerable<int> Calories);
+    private record Elf(IReadOnlyCollection<int> Calories);
+
+    private class ElfBuilder
+    {
+        private readonly List<int> _calories = new();
+
+        public void Add(int calorie)
+        {
+            _calories.Add(calorie);
+        }
+
+        public Elf Build() => new(_calories);
+    }
 }
