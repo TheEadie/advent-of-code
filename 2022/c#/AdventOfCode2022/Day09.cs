873e5da3ebbbdf0ad08d390ba22674c87bffeafc
David Eadie <david@eadie.net>
2022-12-09 21:56:27 +0000

    2022 - Day 9 even more refactoring

--- 2022/c#/AdventOfCode2022/Day09.cs
+++ 2022/c#/AdventOfCode2022/Day09.cs
@@ -6,16 +6,15 @@ public class Day09
     [TestCase("data/09 - Puzzle Input.txt", 5735, TestName = "Puzzle Input")]
     public void Part1(string inputFile, int expected)
     {
-        var start = new Rope(new Coordinate(0, 0), Enumerable.Repeat(new Coordinate(0, 0), 1));
+        var start = Rope.Create(1);
 
         var answer = ParseInput(File.ReadAllText(inputFile))
-            .Aggregate(new List<Rope> {start},
+            .Aggregate(
+                new List<Rope> { start },
                 (ropes, vector) =>
-                {
-                    ropes.Add(Move(ropes.Last(), vector));
-                    return ropes;
-                })
-            .DistinctBy(x => x.Tail.Last()).Count();
+                    ropes.Append(Move(ropes.Last(), vector)).ToList())
+            .DistinctBy(x => x.Knots.Last())
+            .Count();
 
         Console.WriteLine(answer);
         answer.ShouldBe(expected);
@@ -26,16 +25,15 @@ public class Day09
     [TestCase("data/09 - Puzzle Input.txt", 2478, TestName = "Part 2 - Puzzle Input")]
     public void Part2(string inputFile, int expected)
     {
-        var start = new Rope(new Coordinate(0, 0), Enumerable.Repeat(new Coordinate(0,0), 9));
+        var start = Rope.Create(9);
 
         var answer = ParseInput(File.ReadAllText(inputFile))
-            .Aggregate(new List<Rope> {start},
+            .Aggregate(
+                new List<Rope> { start },
                 (ropes, vector) =>
-                {
-                    ropes.Add(Move(ropes.Last(), vector));
-                    return ropes;
-                })
-            .DistinctBy(x => x.Tail.Last()).Count();
+                    ropes.Append(Move(ropes.Last(), vector)).ToList())
+            .DistinctBy(x => x.Knots.Last())
+            .Count();
 
         Console.WriteLine(answer);
         answer.ShouldBe(expected);
@@ -43,21 +41,22 @@ public class Day09
 
     private static Rope Move(Rope rope, Vector vector)
     {
-        var newHead = new Coordinate(rope.Head.X + vector.X, rope.Head.Y + vector.Y);
+        static bool IsAdjacent(Vector diff) =>
+            Math.Abs(diff.X) <= 1 && Math.Abs(diff.Y) <= 1;
+
+        static Coordinate Move(Coordinate current, Vector vector) =>
+            new(current.X + Math.Clamp(vector.X, -1, 1),
+                current.Y + Math.Clamp(vector.Y, -1, 1));
+
+        var newHead = Move(rope.Head, vector);
         var knots = new List<Coordinate>();
         var prevKnot = newHead;
 
-        foreach (var currentTail in rope.Tail)
+        foreach (var currentKnot in rope.Knots)
         {
-            var diff = new Vector(prevKnot.X - currentTail.X, prevKnot.Y - currentTail.Y);
-
-            var newKnot = Math.Abs(diff.X) > 1 || Math.Abs(diff.Y) > 1 ? 
-                new Coordinate(currentTail.X + Math.Clamp(diff.X, -1, 1), 
-                    currentTail.Y + Math.Clamp(diff.Y, -1, 1)) :
-                currentTail;
-            
+            var diff = new Vector(prevKnot.X - currentKnot.X, prevKnot.Y - currentKnot.Y);
+            var newKnot = IsAdjacent(diff) ? currentKnot : Move(currentKnot, diff);
             knots.Add(newKnot);
-
             prevKnot = newKnot;
         }
 
@@ -67,20 +66,24 @@ public class Day09
     private static IEnumerable<Vector> ParseInput(string input)
     {
         return input.Split("\n")
-            .SelectMany(line => 
+            .SelectMany(line =>
                 Enumerable.Repeat(line[0], int.Parse(line[2..]))
-            .Select(x => 
-                x switch
-                {
-                    'U' => new Vector(0, -1),
-                    'D' => new Vector(0, 1),
-                    'L' => new Vector(-1, 0),
-                    'R' => new Vector(1, 0),
-                    _ => throw new ArgumentOutOfRangeException()
-                }));
+                .Select(x =>
+                    x switch
+                    {
+                        'U' => new Vector(0, -1),
+                        'D' => new Vector(0, 1),
+                        'L' => new Vector(-1, 0),
+                        'R' => new Vector(1, 0),
+                        _ => throw new ArgumentOutOfRangeException()
+                    }));
     }
 
-    private record Rope(Coordinate Head, IEnumerable<Coordinate> Tail);
+    private record Rope(Coordinate Head, IEnumerable<Coordinate> Knots)
+    {
+        public static Rope Create(int length) =>
+            new(new Coordinate(0, 0), Enumerable.Repeat(new Coordinate(0, 0), length));
+    }
 
     private record Coordinate(int X, int Y);
 
