b135bf941faaca8fa9b40d387965f835d9338502
David Eadie <david@eadie.net>
2022-12-14 11:11:20 +0000

    2022 - Day 14

---
+++ 2022/c#/AdventOfCode2022/Day14.cs
@@ -0,0 +1,139 @@
+using System.CodeDom;
+
+namespace AdventOfCode2022;
+
+public class Day14
+{
+    [TestCase("data/14 - Sample.txt", 24, TestName = "Sample")]
+    [TestCase("data/14 - Puzzle Input.txt", 858, TestName = "Puzzle Input")]
+    public void Part1(string inputFile, int expected)
+    {
+        var cave = ParseInput(File.ReadAllText(inputFile));
+        var start = new Coordinate(500, 0);
+        var bottomOfCave = cave.Keys.Max(x => x.Y);
+        
+        var answer = DropSand(start, cave)
+            .TakeWhile(x => x.Y < bottomOfCave)
+            .Count();
+        
+        Console.WriteLine(answer);
+        answer.ShouldBe(expected);
+    }
+
+    [TestCase("data/14 - Sample.txt", 93, TestName = "Part 2 - Sample")]
+    [TestCase("data/14 - Puzzle Input.txt", 26845, TestName = "Part 2 - Puzzle Input")]
+    public void Part2(string inputFile, int expected)
+    {
+        var cave = ParseInput(File.ReadAllText(inputFile));
+        var start = new Coordinate(500, 0);
+        
+        var answer = DropSand(start, cave)
+            .TakeUntil(x => x == start)
+            .Count();
+
+        Console.WriteLine(answer);
+        answer.ShouldBe(expected);
+    }
+
+    private static IEnumerable<Coordinate> DropSand(Coordinate start, IDictionary<Coordinate, Item> map)
+    {
+        var current = start;
+        var maxDepth = map.Keys.Max(x => x.Y) + 1;
+
+        while (true)
+        {
+            if (current.Y >= maxDepth)
+            {
+                yield return current;
+                current = start;
+            }
+            
+            var down = current with {Y = current.Y + 1};
+            var downLeft = new Coordinate(current.X -1, current.Y + 1);
+            var downRight = new Coordinate(current.X + 1, current.Y + 1);
+            if (!map.ContainsKey(down))
+            {
+                map.Remove(current);
+                map.Add(down, Item.Sand);
+                current = down;
+            }
+            else if (!map.ContainsKey(downLeft))
+            {
+                map.Remove(current);
+                map.Add(downLeft, Item.Sand);
+                current = downLeft;
+            }
+            else if (!map.ContainsKey(downRight))
+            {
+                map.Remove(current);
+                map.Add(downRight, Item.Sand);
+                current = downRight;
+            }
+            else
+            {
+                yield return current;
+                current = start;
+            }
+        }
+    }
+
+    private static IDictionary<Coordinate, Item> ParseInput(string input)
+    {
+        return input.Split("\n")
+            .SelectMany(x =>
+            {
+                var coordinates = x.Split(" -> ")
+                    .Select(s =>
+                    {
+                        var numbers = s.Split(",").Select(int.Parse).ToArray();
+                        return new Coordinate(numbers[0], numbers[1]);
+                    }).ToList();
+                
+                return coordinates
+                    .Zip(coordinates.Skip(1))
+                    .Select(c => new Line(c.First, c.Second));
+            })
+            .SelectMany(x => x.GetCoordinatesOnLine())
+            .Distinct()
+            .ToDictionary(x => x, _ => Item.Rock);
+    }
+
+    private class Line
+    {
+        public Coordinate Start { get; }
+        public Coordinate End { get; }
+
+        public Line(Coordinate start, Coordinate end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public IEnumerable<Coordinate> GetCoordinatesOnLine()
+        {
+            var xDiff = End.X - Start.X;
+            var yDiff = End.Y - Start.Y;
+            var xMove = xDiff == 0 ? 0 : xDiff > 0 ? 1 : -1;
+            var yMove = yDiff == 0 ? 0 : yDiff > 0 ? 1 : -1;
+
+            var coordinates = new List<Coordinate>();
+            var current = new Coordinate(Start.X, Start.Y);
+            while (current != End)
+            {
+                coordinates.Add(current);
+                current = new Coordinate(current.X + xMove, current.Y + yMove);
+            }
+
+            coordinates.Add(current);
+            return coordinates;
+        }
+    }
+    
+    private record Coordinate(int X, int Y);
+    
+    private enum Item
+    {
+        Rock,
+        Sand
+    }
+}
