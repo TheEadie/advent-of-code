0976d229c386d91a116efe81de9461e0afdd650b
David Eadie <david@eadie.net>
2022-12-15 11:44:08 +0000

    2022 - Day 15 refactoring

--- 2022/c#/AdventOfCode2022/Day15.cs
+++ 2022/c#/AdventOfCode2022/Day15.cs
@@ -8,26 +8,15 @@ public class Day15
     [TestCase("data/15 - Puzzle Input.txt", 2_000_000, 5_508_234, TestName = "Puzzle Input")]
     public void Part1(string inputFile, int y, int expected)
     {
-        var sensors = ParseInput(File.ReadAllText(inputFile))
-            .Select(x => new {Sensor = x, Range = GetDistance(x.Position, x.FirstSensor)})
-            .ToList();
+        var sensors = ParseInput(File.ReadAllText(inputFile)).ToList();
 
-        var minX = sensors.Min(x => x.Sensor.Position.X - x.Range);
-        var maxX = sensors.Max(x => x.Sensor.Position.X + x.Range);
+        var minX = sensors.Min(x => x.Position.X - x.Distance);
+        var maxX = sensors.Max(x => x.Position.X + x.Distance);
 
-        var found = new List<Coordinate>();
-        for (var i = minX; i < maxX; i++)
-        {
-            var test = new Coordinate(i, y);
-            if (sensors
-                .Any(x => 
-                    x.Range >= GetDistance(test, x.Sensor.Position) &&
-                    test != x.Sensor.FirstSensor))
-                found.Add(test);
-        }
+        var answer = Enumerable.Range(minX, (maxX - minX))
+            .Select(x => new Coordinate(x, y))
+            .Count(x => sensors.Any(s => x != s.FirstSensor && s.Distance >= GetDistance(x, s.Position)));
         
-        var answer = found.Count;
-
         Console.WriteLine(answer);
         answer.ShouldBe(expected);
     }
@@ -36,37 +25,30 @@ public class Day15
     [TestCase("data/15 - Puzzle Input.txt", 4_000_000, 10_457_634_860_779, TestName = "Part 2 - Puzzle Input")]
     public void Part2(string inputFile, int maxXY, long expected)
     {
-        var sensors = ParseInput(File.ReadAllText(inputFile))
-            .Select(x => new {Sensor = x, Range = GetDistance(x.Position, x.FirstSensor)})
-            .ToList();
-
-
-        var borderCoordinates = sensors.SelectMany(s =>
-        {
-            var border = new List<Coordinate>();
-            var yOffset = s.Range + 1;
-
-            for (var xOffset = 0; xOffset <= s.Range + 1; xOffset++)
-            {
-                border.Add(new Coordinate(s.Sensor.Position.X + xOffset, s.Sensor.Position.Y + yOffset));
-                border.Add(new Coordinate(s.Sensor.Position.X + xOffset, s.Sensor.Position.Y - yOffset));
-                border.Add(new Coordinate(s.Sensor.Position.X - xOffset, s.Sensor.Position.Y + yOffset));
-                border.Add(new Coordinate(s.Sensor.Position.X - xOffset, s.Sensor.Position.Y - yOffset));
-                yOffset--;
-            }
-
-            return border;
-        });
-
-        var found = borderCoordinates
-            .Distinct()
+        var sensors = ParseInput(File.ReadAllText(inputFile)).ToList();
+        var found = sensors
+            .SelectMany(GetBorder)
             .Where(x => x.X > 0 && x.X <= maxXY && x.Y > 0 && x.Y <= maxXY)
-            .Single(x => sensors.All(s => s.Range < GetDistance(x, s.Sensor.Position)));
+            .First(x => sensors.All(s => s.Distance < GetDistance(x, s.Position)));
         
         var answer = (long)found.X * 4_000_000 + found.Y;
 
         Console.WriteLine(answer);
         answer.ShouldBe(expected);
+    }
+
+    private static IEnumerable<Coordinate> GetBorder(Sensor sensor)
+    {
+        var yOffset = sensor.Distance + 1;
+
+        for (var xOffset = 0; xOffset <= sensor.Distance + 1; xOffset++)
+        {
+            yield return new Coordinate(sensor.Position.X + xOffset, sensor.Position.Y + yOffset);
+            yield return new Coordinate(sensor.Position.X + xOffset, sensor.Position.Y - yOffset);
+            yield return new Coordinate(sensor.Position.X - xOffset, sensor.Position.Y + yOffset);
+            yield return new Coordinate(sensor.Position.X - xOffset, sensor.Position.Y - yOffset);
+            yOffset--;
+        }
     }
 
     private IEnumerable<Sensor> ParseInput(string input)
@@ -77,9 +59,10 @@ public class Day15
                 "Sensor at x=([-|0-9]+), y=([-|0-9]+): closest beacon is at x=([-|0-9]+), y=([-|0-9]+)");
             var groups = regex.Match(line).Groups;
 
-            return new Sensor(
-                new Coordinate(int.Parse(groups[1].Value), int.Parse(groups[2].Value)),
-                new Coordinate(int.Parse(groups[3].Value), int.Parse(groups[4].Value)));
+            var pos = new Coordinate(int.Parse(groups[1].Value), int.Parse(groups[2].Value));
+            var firstBeacon = new Coordinate(int.Parse(groups[3].Value), int.Parse(groups[4].Value));
+            
+            return new Sensor(pos, firstBeacon, GetDistance(pos, firstBeacon));
         }
         
         return input.Split("\n").Select(ParseSensor);
@@ -90,5 +73,5 @@ public class Day15
         return Math.Abs(end.X - start.X) + Math.Abs(end.Y - start.Y);
     }
 
-    private record Sensor(Coordinate Position, Coordinate FirstSensor);
+    private record Sensor(Coordinate Position, Coordinate FirstSensor, int Distance);
 }
