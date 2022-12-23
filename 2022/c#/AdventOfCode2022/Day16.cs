5b152999e4546111bc932e41841364ed4442824d
David Eadie <david@eadie.net>
2022-12-23 14:43:45 +0000

    WIP

--- 2022/c#/AdventOfCode2022/Day16.cs
+++ 2022/c#/AdventOfCode2022/Day16.cs
@@ -9,7 +9,7 @@ public class Day16
     public void Part1(string inputFile, int expected)
     {
         var input = ParseInput(File.ReadAllText(inputFile)).ToList();
-        var answer = FindRoute(input, 2);
+        var answer = FindRoute(input, 50);
 
         Console.WriteLine(answer);
         answer.ShouldBe(expected);
@@ -26,216 +26,67 @@ public class Day16
         answer.ShouldBe(expected);
         // 2557 - Too low
     }
-/*
-    private int FindRouteWith2(List<Valve> input, int branches)
-    {
-        const int time = 26;
-
-        var statesToTry = new HashSet<(State, State)>();
-        var allStates = new HashSet<(State, State)>();
-        var finished = new List<(State, State)>();
-
-        statesToTry.Add((new State(input.Single(x => x.Id == "AA"), new List<Valve>(), 0, 0, 0),
-            new State(input.Single(x => x.Id == "AA"), new List<Valve>(), 0, 0, 0)));
-
-        while (statesToTry.Any())
-        {
-            var current = statesToTry.ElementAt(0);
-            statesToTry.Remove(current);
-
-            var nextTime = current.Item1.Minute + 1;
-            var nextFlowOne = current.Item1.Flow + current.Item1.OpenValves.Sum(x => x.FlowRate);
-            var nextFlowTwo = current.Item2.Flow + current.Item2.OpenValves.Sum(x => x.FlowRate);
-            var optionsOne = ScoreValves(input, current.Item1.CurrentRoom, time - current.Item1.Minute)
-                .Where(x => !current.Item1.OpenValves.Concat(current.Item2.OpenValves).Contains(x.Key))
-                .OrderByDescending(x => x.Value.Item1)
-                .Take(branches)
-                .ToList();
-
-            var optionsTwo = ScoreValves(input, current.Item2.CurrentRoom, time - current.Item2.Minute)
-                .Where(x => !current.Item1.OpenValves.Concat(current.Item2.OpenValves).Contains(x.Key))
-                .OrderByDescending(x => x.Value.Item1)
-                .Take(branches)
-                .ToList();
-
-            var nextStatesOne = new List<State>();
-            var nextStatesTwo = new List<State>();
-
-            foreach (var option in optionsOne)
-            {
-                var nextRoom = current.Item1.CurrentRoom;
-                var nextOpenValves = current.Item1.OpenValves.ToList();
-
-                if (option.Key == current.Item1.CurrentRoom)
-                {
-                    nextOpenValves.Add(option.Key);
-                }
-                else
-                {
-                    nextRoom = option.Value.Item2.Skip(1).First();
-                }
-
-                nextStatesOne.Add(new State(nextRoom, nextOpenValves, nextTime, nextFlowOne, option.Value.Item1));
-            }
-
-            foreach (var option in optionsTwo)
-            {
-                var nextRoom = current.Item2.CurrentRoom;
-                var nextOpenValves = current.Item2.OpenValves.ToList();
-
-                if (option.Key == current.Item2.CurrentRoom)
-                {
-                    nextOpenValves.Add(option.Key);
-                }
-                else
-                {
-                    nextRoom = option.Value.Item2.Skip(1).First();
-                }
-
-                nextStatesTwo.Add(new State(nextRoom, nextOpenValves, nextTime, nextFlowTwo, option.Value.Item1));
-            }
-
-            foreach (var optionOne in nextStatesOne)
-            {
-                foreach (var optionTwo in nextStatesTwo)
-                {
-                    if (optionOne.CurrentRoom.Id == optionTwo.CurrentRoom.Id)
-                        continue;
-
-                    if (nextTime < time)
-                    {
-                        if (statesToTry.Any(x =>
-                                x.Item1.Minute == nextTime &&
-                                x.Item1.CurrentRoom.Id == optionOne.CurrentRoom.Id &&
-                                x.Item2.CurrentRoom.Id == optionTwo.CurrentRoom.Id))
-                            continue;
-
-                        var otherOptions = statesToTry
-                            .Where(x => x.Item1.Minute == nextTime)
-                            .ToList();
-
-                        var topOptions = otherOptions
-                            .Append((optionOne, optionTwo))
-                            .OrderByDescending(x => (x.Item1.Flow + x.Item2.Flow + x.Item1.Potential + x.Item2.Potential))
-                            .Take(branches);
-
-                        foreach (var other in otherOptions)
-                        {
-                            statesToTry.Remove(other);
-                            allStates.Remove(other);
-                        }
-
-                        foreach (var top in topOptions)
-                        {
-                            statesToTry.Add(top);
-                            allStates.Add(top);
-                        }
-                    }
-                    else
-                    {
-                        var totalFlow = optionOne.Flow + optionTwo.Flow;
-                        var totalOpenValves = optionOne.OpenValves.Concat(optionTwo.OpenValves);
-                        //Console.WriteLine(
-                        //    $"Finished: {totalFlow} Me: {string.Join(", ", optionOne.Item1.OpenValves.Select(x => x.Id))}  " +
-                        //    $"Elephant: {string.Join(", ", optionTwo.Item1.OpenValves.Select(x => x.Id))}");
-                        finished.Add((optionOne, optionTwo));
-                    }
-                }
-            }
-        }
-
-        foreach (var state in allStates.OrderBy(x => x.Item1.Minute))
-        {
-            Console.WriteLine($"Minute {state.Item1.Minute} " +
-                              $"me: {state.Item1.CurrentRoom.Id}, " +
-                              $"Elephant: {state.Item2.CurrentRoom.Id}, " +
-                              $"Score: {state.Item1.Flow + state.Item2.Flow + state.Item1.Potential + state.Item2.Potential}");
-        }
-
-        return finished.Max(x => x.Item1.Flow + x.Item2.Flow);
-    }*/
 
     private int FindRoute(List<Valve> input, int branches)
     {
-        var statesToTry = new HashSet<State>();
-        var finished = new List<State>();
+        var valves = input.ToDictionary(k => k.Id, v => v.FlowRate);
+        valves.Add("",0);
+        
+        var statesToTry = new Queue<State>();
+        var statesTried = new HashSet<State>();
 
-        statesToTry.Add(new State("AA", "", 0, 0, 0));
+        statesToTry.Enqueue(new State("AA", "", 0, 0));
 
         while (statesToTry.Any())
         {
-            var current = statesToTry.ElementAt(0);
-            statesToTry.Remove(current);
+            var current = statesToTry.Dequeue();
+            if (statesTried.Contains(current))
+                continue;
 
-            var nextTime = current.Minute + 1;
-
-            var openValves = current.OpenValves
-                .Split(",")
-                .Where(x => x != "")
-                .Select(x => input.Single(v => v.Id == x))
-                .ToHashSet();
-            var nextTotalFlow = current.TotalFlow + openValves.Sum(x => x.FlowRate);
+            statesTried.Add(current);
             
             var options = ScoreValves(input, current.CurrentRoom, 30 - current.Minute)
                 .Where(x => !current.OpenValves.Contains(x.Key.Id))
-                .OrderByDescending(x => x.Value.Item1)
+                .OrderByDescending(x => x.Value.Item2)
                 .Take(branches)
                 .ToList();
 
             foreach (var option in options)
             {
-                var nextRoom = current.CurrentRoom;
-                var nextOpenValves = current.OpenValves;
+                var nextRoom = option.Key.Id;
+                var nextOpenValves = current.OpenValves + "," + option.Key.Id;
+                var nextTime = current.Minute + option.Value.Item1;
 
-                if (option.Key.Id == current.CurrentRoom)
-                {
-                    nextOpenValves += "," + option.Key.Id;
-                }
-                else
-                {
-                    nextRoom = option.Value.Item2.Skip(1).First();
-                }
+                var flow = current.OpenValves.Split(",").Sum(x => valves[x]);
+                var nextTotalFlow = current.TotalFlow + (flow * option.Value.Item1);
 
-                var nextState = new State(nextRoom, nextOpenValves, nextTime, nextTotalFlow, option.Value.Item1);
+                var nextState = new State(nextRoom, nextOpenValves, nextTime, nextTotalFlow);
 
                 if (nextTime < 30)
                 {
-                    var otherOptions = statesToTry
-                        .Where(x => x.Minute == nextTime)
-                        .ToList();
-
-                    var topOptions = otherOptions
-                        .Append(nextState)
-                        .OrderByDescending(x => x.TotalFlow)
-                        .Take(branches);
-
-                    foreach (var other in otherOptions)
+                    if (!statesTried.Contains(nextState))
                     {
-                        statesToTry.Remove(other);
-                    }
-
-                    foreach (var top in topOptions)
-                    {
-                        statesToTry.Add(top);
+                        statesToTry.Enqueue(nextState);
                     }
                 }
                 else
                 {
-                    Console.WriteLine(
-                        $"Finished: {nextState.TotalFlow} {nextState.OpenValves}");
-                    finished.Add(nextState);
+                    //Console.WriteLine(
+                    //    $"Finished: {nextState.TotalFlow} {nextState.OpenValves}");
                 }
             }
         }
 
-        return finished.Max(x => x.TotalFlow);
+        var lastState = statesTried.MaxBy(x => x.TotalFlow + ((30 - x.Minute) * x.OpenValves.Split(",").Sum(v => valves[v])));
+        Console.WriteLine($"{lastState.TotalFlow} - {lastState.OpenValves}");
+        return lastState.TotalFlow + ((30 - lastState.Minute) * lastState.OpenValves.Split(",").Sum(v => valves[v]));
     }
 
-    private IDictionary<Valve, (double, IEnumerable<string>)> ScoreValves(IList<Valve> valves, string currentRoom, int timeLeft)
+    private IDictionary<Valve, (int, double)> ScoreValves(IList<Valve> valves, string currentRoom, int timeLeft)
     {
-        var found = new Dictionary<Valve, (double, IEnumerable<string>)>();
+        var found = new Dictionary<Valve, (int, double)>();
 
-        foreach (var valve in valves)
+        foreach (var valve in valves.Where(x => x.FlowRate > 0))
         {
             var (distance, path) = PathFinding.AStar(currentRoom,
                 valve.Id,
@@ -244,7 +95,7 @@ public class Day16
                 (_, _) => 0);
 
             var opportunity = (double)(timeLeft - distance - 1) * valve.FlowRate;
-            found.Add(valve, (opportunity / (distance + 1), path));
+            found.Add(valve, (distance + 1, opportunity));
         }
 
         return found;
@@ -269,8 +120,7 @@ public class Day16
         string CurrentRoom, 
         string OpenValves, 
         int Minute, 
-        int TotalFlow,
-        double Potential);
+        int TotalFlow);
 
     private record Valve(string Id, int FlowRate, IEnumerable<string> Tunnels);
 }
