270fa7051f86e4726b9a61d371654712e96af105
David Eadie <david@eadie.net>
2022-12-26 19:14:55 +0000

    2022 - Day 16 BFS

--- 2022/c#/AdventOfCode2022/Day16.cs
+++ 2022/c#/AdventOfCode2022/Day16.cs
@@ -9,14 +9,14 @@ public class Day16
     public void Part1(string inputFile, int expected)
     {
         var input = ParseInput(File.ReadAllText(inputFile)).ToList();
-        var answer = FindRoute(input, 50);
+        var answer = FindRoute(input);
 
         Console.WriteLine(answer);
         answer.ShouldBe(expected);
     }
 
-    [TestCase("data/16 - Sample.txt", 1707, TestName = "Part 2 - Sample")]
-    [TestCase("data/16 - Puzzle Input.txt", 0, TestName = "Part 2 - Puzzle Input")]
+    //[TestCase("data/16 - Sample.txt", 1707, TestName = "Part 2 - Sample")]
+    //[TestCase("data/16 - Puzzle Input.txt", 0, TestName = "Part 2 - Puzzle Input")]
     public void Part2(string inputFile, int expected)
     {
         var input = ParseInput(File.ReadAllText(inputFile)).ToList();
@@ -27,11 +27,39 @@ public class Day16
         // 2557 - Too low
     }
 
-    private int FindRoute(List<Valve> input, int branches)
+    private static IDictionary<string, Dictionary<Valve, int>> GetDistances(List<Valve> valves)
     {
-        var valves = input.ToDictionary(k => k.Id, v => v.FlowRate);
-        valves.Add("",0);
-        
+        var distances = new Dictionary<string, Dictionary<Valve, int>>();
+
+        foreach (var v in valves.Where(x => x.FlowRate > 0 || x.Id == "AA"))
+        {
+            var found = new Dictionary<Valve, int>();
+
+            foreach (var valve in valves.Where(x => x.FlowRate > 0))
+            {
+                var (distance, path) = PathFinding.AStar(v.Id,
+                    valve.Id,
+                    node => valves.Where(x => x.Tunnels.Contains(node)).Select(x => x.Id),
+                    (_, _) => 1,
+                    (_, _) => 0);
+
+                found.Add(valve, distance + 1);
+            }
+
+            distances.Add(v.Id, found);
+        }
+
+        return distances;
+    }
+
+    private static int FindRoute(List<Valve> input)
+    {
+        var flowRates = input.ToDictionary(k => k.Id, v => v.FlowRate);
+        flowRates.Add("", 0);
+
+        var distances = GetDistances(input);
+        var neighbours = distances.ToDictionary(k => k.Key, v => v.Value.Keys);
+
         var statesToTry = new Queue<State>();
         var statesTried = new HashSet<State>();
 
@@ -40,25 +68,23 @@ public class Day16
         while (statesToTry.Any())
         {
             var current = statesToTry.Dequeue();
-            if (statesTried.Contains(current))
-                continue;
 
             statesTried.Add(current);
-            
-            var options = ScoreValves(input, current.CurrentRoom, 30 - current.Minute)
-                .Where(x => !current.OpenValves.Contains(x.Key.Id))
-                .OrderByDescending(x => x.Value.Item2)
-                .Take(branches)
+
+            var costs = distances[current.CurrentRoom];
+
+            var options = neighbours[current.CurrentRoom]
+                .Where(x => !current.OpenValves.Contains(x.Id))
                 .ToList();
 
             foreach (var option in options)
             {
-                var nextRoom = option.Key.Id;
-                var nextOpenValves = current.OpenValves + "," + option.Key.Id;
-                var nextTime = current.Minute + option.Value.Item1;
+                var nextRoom = option.Id;
+                var nextOpenValves = current.OpenValves + "," + option.Id;
+                var nextTime = current.Minute + costs[option];
 
-                var flow = current.OpenValves.Split(",").Sum(x => valves[x]);
-                var nextTotalFlow = current.TotalFlow + (flow * option.Value.Item1);
+                var flow = current.OpenValves.Split(",").Sum(x => flowRates[x]);
+                var nextTotalFlow = current.TotalFlow + (flow * costs[option]);
 
                 var nextState = new State(nextRoom, nextOpenValves, nextTime, nextTotalFlow);
 
@@ -71,34 +97,25 @@ public class Day16
                 }
                 else
                 {
-                    //Console.WriteLine(
-                    //    $"Finished: {nextState.TotalFlow} {nextState.OpenValves}");
+                    statesTried.Add(nextState with
+                    {
+                        Minute = 30,
+                        TotalFlow = current.TotalFlow + ((30 - current.Minute) * flow)
+                    });
                 }
             }
         }
 
-        var lastState = statesTried.MaxBy(x => x.TotalFlow + ((30 - x.Minute) * x.OpenValves.Split(",").Sum(v => valves[v])));
-        Console.WriteLine($"{lastState.TotalFlow} - {lastState.OpenValves}");
-        return lastState.TotalFlow + ((30 - lastState.Minute) * lastState.OpenValves.Split(",").Sum(v => valves[v]));
-    }
+        var finishedStates = statesTried
+            .Select(x => x with
+            {
+                Minute = 30,
+                TotalFlow = x.TotalFlow + (Math.Max(30 - x.Minute, 0) * x.OpenValves.Split(",").Sum(x => flowRates[x]))
+            });
 
-    private IDictionary<Valve, (int, double)> ScoreValves(IList<Valve> valves, string currentRoom, int timeLeft)
-    {
-        var found = new Dictionary<Valve, (int, double)>();
-
-        foreach (var valve in valves.Where(x => x.FlowRate > 0))
-        {
-            var (distance, path) = PathFinding.AStar(currentRoom,
-                valve.Id,
-                node => valves.Where(x => x.Tunnels.Contains(node)).Select(x => x.Id),
-                (_, _) => 1,
-                (_, _) => 0);
-
-            var opportunity = (double)(timeLeft - distance - 1) * valve.FlowRate;
-            found.Add(valve, (distance + 1, opportunity));
-        }
-
-        return found;
+        var lastState = finishedStates.MaxBy(x => x.TotalFlow);
+        Console.WriteLine($"{lastState}");
+        return lastState!.TotalFlow;
     }
 
     private IEnumerable<Valve> ParseInput(string input)
@@ -117,9 +134,9 @@ public class Day16
     }
 
     private record State(
-        string CurrentRoom, 
-        string OpenValves, 
-        int Minute, 
+        string CurrentRoom,
+        string OpenValves,
+        int Minute,
         int TotalFlow);
 
     private record Valve(string Id, int FlowRate, IEnumerable<string> Tunnels);
