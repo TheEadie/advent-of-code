8e96025ebff212e42e991cf0ea0e2967d5196674
David Eadie <david@eadie.net>
2023-12-05 15:26:37 +0000

    2023 - Day 5 cleanup

--- csharp/2023/Day05/Day05.cs
+++ csharp/2023/Day05/Day05.cs
@@ -15,14 +15,10 @@ public class Day05
         var input = await _session.Start(inputFile);
         var almanac = Parse(input);
 
-        var answer = almanac.Seeds.Select(x => ApplyMap(x, almanac.Maps[0]))
-            .Select(x => ApplyMap(x, almanac.Maps[1]))
-            .Select(x => ApplyMap(x, almanac.Maps[2]))
-            .Select(x => ApplyMap(x, almanac.Maps[3]))
-            .Select(x => ApplyMap(x, almanac.Maps[4]))
-            .Select(x => ApplyMap(x, almanac.Maps[5]))
-            .Select(x => ApplyMap(x, almanac.Maps[6]))
-            .MinBy(x => x);
+        var answer = almanac.Maps
+            .Aggregate(almanac.Seeds, (current, map) =>
+                current.Select(x => ApplyMap(x, map)).ToList())
+            .Min();
 
         _session.PrintAnswer(1, answer);
         answer.ShouldBe(expected);
@@ -35,53 +31,42 @@ public class Day05
         var input = await _session.Start(inputFile);
         var almanac = Parse(input);
 
-        var currentRanges = almanac.Seeds.Chunk(2).Select(x => (Start: x[0], End: x[0] + x[1] - 1));
+        var seedRanges = almanac.Seeds
+            .Chunk(2)
+            .Select(x => new Range(x[0], x[0] + x[1] - 1));
 
-        var inputRanges = almanac.Maps.Select(
-                x => x.Select(m => (m.SourceStart, m.SourceStart + m.Length - 1))
-                    .Concat(
-                        new List<(long, long)>
-                        {
-                            (0L, x.Min(m => m.SourceStart)),
-                            (x.Max(m => m.SourceStart + m.Length - 1), long.MaxValue)
-                        }))
-            .ToList();
-
-        for (var i = 0; i < inputRanges.Count; i++)
-        {
-            currentRanges = Intersect(currentRanges, inputRanges[i])
-                .Select(x => (ApplyMap(x.Item1, almanac.Maps[i]), ApplyMap(x.Item2, almanac.Maps[i])))
-                .ToList();
-        }
-
-        var answer = currentRanges.Min(x => x.Start);
+        var answer = almanac.Maps
+            .Aggregate(seedRanges, (current, map) =>
+                    Intersect(current, map.Select(x => x.Source))
+                        .Select(x => new Range(ApplyMap(x.Start, map), ApplyMap(x.End, map)))
+                        .ToList())
+            .Min(x => x.Start);
 
         _session.PrintAnswer(2, answer);
         answer.ShouldBe(expected);
     }
 
-    private static IEnumerable<(long, long)> Intersect(IEnumerable<(long, long)> a, IEnumerable<(long, long)> b) =>
+    private static IEnumerable<Range> Intersect(IEnumerable<Range> a, IEnumerable<Range> b) =>
         a.SelectMany(x => b.SelectMany(y => Intersect(x, y)));
 
-    private static IEnumerable<(long, long)> Intersect((long, long) a, (long, long) b)
+    private static IEnumerable<Range> Intersect(Range a, Range b)
     {
-        if (a.Item1 > b.Item2 || a.Item2 < b.Item1)
+        if (a.Start > b.End || a.End < b.Start)
         {
             yield break;
         }
 
-        yield return (Math.Max(a.Item1, b.Item1), Math.Min(a.Item2, b.Item2));
+        yield return new Range(Math.Max(a.Start, b.Start), Math.Min(a.End, b.End));
     }
 
     private static long ApplyMap(long value, IEnumerable<Map> maps)
     {
-        var map = maps.FirstOrDefault(x => value >= x.SourceStart && value < x.SourceStart + x.Length);
-        if (map is null)
+        var map = maps.FirstOrDefault(x => value >= x.Source.Start && value <= x.Source.End);
+        if (map == null)
         {
             return value;
         }
-
-        return map.DestinationStart + (value - map.SourceStart);
+        return map.Destination.Start + (value - map.Source.Start);
     }
 
     private static Almanac Parse(string input)
@@ -89,16 +74,35 @@ public class Day05
         var sections = input.Split("\n\n");
         return new Almanac(
             sections[0].Split(" ").Skip(1).Select(long.Parse).ToList(),
-            sections.Skip(1).Select(x => x.Split("\n").Skip(1).Select(ParseMap)).ToList());
+            sections.Skip(1).Select(x =>
+                {
+                    var parsedMaps = x.Split("\n").Skip(1).Select(ParseMap).ToList();
+                    parsedMaps.AddRange(
+                        new List<Map>()
+                        {
+                            new(
+                                new Range(0L, parsedMaps.Min(m => m.Source.Start - 1)),
+                                new Range(0L, parsedMaps.Min(m => m.Destination.Start - 1))),
+                            new(
+                                new Range(parsedMaps.Max(m => m.Source.End + 1), long.MaxValue),
+                                new Range(parsedMaps.Max(m => m.Destination.End + 1), long.MaxValue))
+                        });
+                    return parsedMaps;
+                }).ToList()
+            );
     }
 
     private static Map ParseMap(string input)
     {
         var parts = input.Split(" ");
-        return new Map(long.Parse(parts[0]), long.Parse(parts[1]), long.Parse(parts[2]));
+        return new Map(
+            new Range(long.Parse(parts[1]), long.Parse(parts[1]) + long.Parse(parts[2]) - 1),
+            new Range(long.Parse(parts[0]), long.Parse(parts[0]) + long.Parse(parts[2]) - 1));
     }
 
-    private record Almanac(IList<long> Seeds, IReadOnlyList<IEnumerable<Map>> Maps);
+    private record Almanac(IList<long> Seeds, IReadOnlyList<IReadOnlyList<Map>> Maps);
 
-    private record Map(long DestinationStart, long SourceStart, long Length);
+    private record Map(Range Source, Range Destination);
+
+    private record Range(long Start, long End);
 }
