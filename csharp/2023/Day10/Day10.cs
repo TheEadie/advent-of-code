794910792c4d9698ab6675c283106e36e80bc15d
David Eadie <david@eadie.net>
2024-03-28 15:44:02 +0000

    2023 - Day 10 Part 2

--- csharp/2023/Day10/Day10.cs
+++ csharp/2023/Day10/Day10.cs
@@ -9,14 +9,86 @@ public class Day10
     public void SetUp() => _session.PrintHeading();
 
     [TestCase("Sample.txt", 8)]
-    [TestCase("Puzzle Input.txt", 0)]
+    [TestCase("Puzzle Input.txt", 7102)]
     public async Task Part1(string inputFile, int expected)
     {
         var input = await _session.Start(inputFile);
-        var map = Parse(input);
+        var (start, map) = Parse(input);
+        var loop = GetLoop(start, map);
 
-        var start = map.Single(x => x.Value.Intersect(Vector.FourDirections()).Count() == 4).Key;
+        var answer = loop.Values.Max() / 3;
 
+        _session.PrintAnswer(1, answer);
+        answer.ShouldBe(expected);
+    }
+
+    [TestCase("Sample.txt", 1)]
+    [TestCase("Puzzle Input.txt", 363)]
+    public async Task Part2(string inputFile, int expected)
+    {
+        var input = await _session.Start(inputFile);
+        var (start, map) = Parse(input);
+        var bottomRight = new Coordinate(map.Max(x => x.X) + 1, map.Max(x => x.Y) + 1);
+
+        var loop = GetLoop(start, map);
+        var outside = FloodFill(loop.Keys.ToHashSet(), new Coordinate(0,0), bottomRight);
+
+        var answer = 0;
+
+        for (var y = 0; y < bottomRight.Y / 3; y++)
+        {
+            for (var x = 0; x < bottomRight.X / 3; x++)
+            {
+                var middle = new Coordinate(x * 3 + 1, y * 3 + 1);
+                if (!outside.Contains(middle) &&
+                    !loop.ContainsKey(middle))
+                {
+                    answer++;
+                }
+            }
+        }
+
+        _session.PrintAnswer(2, answer);
+        answer.ShouldBe(expected);
+    }
+
+    private static IReadOnlySet<Coordinate> FloodFill(IReadOnlySet<Coordinate> map, Coordinate startPoint, Coordinate bottomRight)
+    {
+        var queue = new Queue<Coordinate>();
+        var visited = new HashSet<Coordinate>();
+
+        queue.Enqueue(startPoint);
+        while(queue.Count != 0)
+        {
+            var current = queue.Dequeue();
+            if (visited.Contains(current))
+            {
+                continue;
+            }
+            _ = visited.Add(current);
+
+            var neighbours = Vector.FourDirections()
+                .Select(x => current + x)
+                .Where(x => x.X >= 0 &&
+                    x.Y >= 0 &&
+                    x.X <= bottomRight.X &&
+                    x.Y <= bottomRight.Y &&
+                    !map.Contains(x));
+
+            foreach (var neighbour in neighbours)
+            {
+                if (!visited.Contains(neighbour))
+                {
+                    queue.Enqueue(neighbour);
+                }
+            }
+        }
+
+        return visited;
+    }
+
+    private static Dictionary<Coordinate, int> GetLoop(Coordinate start, IReadOnlySet<Coordinate> map)
+    {
         var queue = new Queue<Coordinate>();
         var visited = new HashSet<Coordinate>();
         var distances = new Dictionary<Coordinate, int>{ { start, 0 } };
@@ -27,7 +99,10 @@ public class Day10
             var current = queue.Dequeue();
             _ = visited.Add(current);
 
-            var neighbours = GetNeighbours(current, map);
+            var neighbours = Vector.FourDirections()
+                .Select(x => current + x)
+                .Where(map.Contains);
+
             foreach (var neighbour in neighbours)
             {
                 if (visited.Contains(neighbour))
@@ -40,61 +115,36 @@ public class Day10
             }
         }
 
-        var answer = distances.Values.Max();
-
-        _session.PrintAnswer(1, answer);
-        answer.ShouldBe(expected);
-    }
-
-    private static IEnumerable<Coordinate> GetNeighbours(
-        Coordinate position,
-        IDictionary<Coordinate, IEnumerable<Vector>> map)
-    {
-        var directions = map[position];
-
-        foreach (var direction in directions)
-        {
-            var next = position + direction;
-
-            if (!map.ContainsKey(next))
-            {
-                continue;
-            }
-
-            if (direction == Vector.Up && map[next].Contains(Vector.Down) ||
-                direction == Vector.Down && map[next].Contains(Vector.Up) ||
-                direction == Vector.Left && map[next].Contains(Vector.Right) ||
-                direction == Vector.Right && map[next].Contains(Vector.Left))
-            {
-                yield return next;
-            }
-        }
+        return distances;
     }
 
 
-    [TestCase("Sample.txt", 0)]
-    [TestCase("Puzzle Input.txt", 0)]
-    public async Task Part2(string inputFile, int expected)
+    private static (Coordinate, IReadOnlySet<Coordinate>) Parse(string input)
     {
-        var input = await _session.Start(inputFile);
-
-        var answer = 0;
-
-        _session.PrintAnswer(2, answer);
-        answer.ShouldBe(expected);
-    }
-
-    private static IDictionary<Coordinate, IEnumerable<Vector>> Parse(string input)
-    {
+        var map = new HashSet<Coordinate>();
+        var start = new Coordinate(0, 0);
         var lines = input.Split("\n");
 
-        var map = Enumerable.Range(0, lines.Length)
-            .SelectMany(
-                y => Enumerable.Range(0, lines[0].Length)
-                    .Select(x => (Coordinate: new Coordinate(x, y), Value: lines[y][x]))
-                    .Select(x => (x.Coordinate, Vectors: GetVectors(x.Value), Symbol: x.Value)))
-            .ToDictionary(key => key.Coordinate, value => value.Vectors);
-        return map;
+        for (var y = 0; y < lines.Length; y++)
+        {
+            for (var x = 0; x < lines[0].Length; x++)
+            {
+                var c = lines[y][x];
+                var middle = new Coordinate(x * 3 + 1, y * 3 + 1);
+                map.Add(middle);
+                foreach (var vector in GetVectors(c))
+                {
+                    map.Add(middle + vector);
+                }
+
+                if (c == 'S')
+                {
+                    start = middle;
+                }
+            }
+        }
+
+        return (start, map);
     }
 
     private static IEnumerable<Vector> GetVectors(char c) =>
